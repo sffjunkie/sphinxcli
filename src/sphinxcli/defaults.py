b61cfea77d59dbcceafb5887dc45dcd17fdb436b
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-18 20:55:43 +0100

    Update default values

--- src/sphinxcli/defaults.py
+++ src/sphinxcli/defaults.py
@@ -1,8 +1,11 @@
-LIST_SEPARATOR = "|"
+from sphinxcli.types import Builder
+
+PYPROJECT_TABLE_NAME = "sphinxcli"
+LIST_SEPARATOR = ":"
 DEFAULT_SOURCE_PATH = "."
 DEFAULT_TARGET_PATH = "build"
 DEFAULT_TARGET_LANG = "en"
 DEFAULT_LANGUAGES = ["en"]
 DEFAULT_LC_MESSAGES = "en"
-DEFAULT_BUILDERS = ["html"]
+DEFAULT_BUILDERS: list[Builder] = ["html"]
 DEFAULT_TARGET_ORDER = "builder"
