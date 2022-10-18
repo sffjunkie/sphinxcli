e8f515525930a7073286068fda8f97f267275d96
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-18 21:02:40 +0100

    Moved test files

---
+++ src/sphinxcli/sphinxconf.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from typing import Any
+
+
+def dict_to_sphinxconf(config: dict[str, Any]):
+    ...
+
+
+def toml_to_sphinxconf(tomlfile: Path):
+    data = toml_to_doc(tomlfile)
+    return dict_to_sphinxconf(data)
