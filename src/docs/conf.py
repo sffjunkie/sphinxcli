981452eb019177b345c2ceceb8331bffea3bd21b
Simon Kennedy <sffjunkie+code@gmail.com>
2022-10-21 11:09:32 +0100

    Update version number

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -9,7 +9,7 @@
 project = "SphinxCLI"
 copyright = "2022, Simon Kennedy"
 author = "Simon Kennedy"
-release = "0.1.0"
+release = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
